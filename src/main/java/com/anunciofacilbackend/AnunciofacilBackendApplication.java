2113c20c67abc47e193a2a492ce8b7d022787d16
Paul Yauli <paul.yauli@promart.pe>
2025-11-23 20:01:03 -0500

    first commit

---
+++ src/main/java/com/anunciofacilbackend/AnunciofacilBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anunciofacilbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnunciofacilBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnunciofacilBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anunciofacilbackend/AnunciofacilBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anunciofacilbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnunciofacilBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
